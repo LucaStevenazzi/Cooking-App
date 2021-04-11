9ad3ab40e88f8793d38ddfe33281a90699dc40a2
LucaStevenazzi <lstevenazzi@studenti.uninsubria.it>
2021-04-11 20:20:39 +0200

    New branch dev_Luca

--- app/src/main/java/com/example/cooking_app/MainActivity.kt
+++ app/src/main/java/com/example/cooking_app/MainActivity.kt
@@ -7,19 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
-
-
-    //test git push
-
-
-
-
-
-
-
-
-
 
 }
