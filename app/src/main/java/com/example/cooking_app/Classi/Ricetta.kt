10f1786e135f95cfe75de642eb0a3106b641de3c
PietroRedaelli <predaelli@studenti.uninsubria.it>
2021-05-20 17:48:45 +0200

    pre pull

--- app/src/main/java/com/example/cooking_app/Classi/Ricetta.kt
+++ app/src/main/java/com/example/cooking_app/Classi/Ricetta.kt
@@ -6,6 +6,6 @@ Classe Ricette
 data class Ricetta(var immagine: String, var nome: String, var diff: String, var tempo: String, var tipologia: String, var portata: String, var persone: Int,
                    var listaIngredienti: ArrayList<Ingredienti>, var note: String){
 
-    constructor(): this("","Ricetta Default", "", "", "", "", 0, ArrayList(), "")
+    constructor(): this("","Ricetta Default", "", "", "", "", 0, ArrayList(),  "")
 }
 
