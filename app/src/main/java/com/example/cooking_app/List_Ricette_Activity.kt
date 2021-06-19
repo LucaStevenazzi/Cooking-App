64ec39c641cad250ebe49fed0981f1170d89d089
LucaStevenazzi <lstevenazzi@studenti.uninsubria.it>
2021-06-19 15:48:13 +0200

    Commit: - Add testo dell'applicazione in List_Ricette_Activity.kt

--- app/src/main/java/com/example/cooking_app/List_Ricette_Activity.kt
+++ app/src/main/java/com/example/cooking_app/List_Ricette_Activity.kt
@@ -19,7 +19,21 @@ import kotlinx.android.synthetic.main.filtro_ricerca_fragment.*
 import kotlinx.android.synthetic.main.list_ricette_activity.*
 
 /*
-Main Activity con lista di ricette
+Spesso capita che le dosi di una ricetta siano per un numero non adatto alle proprie esigenze, obbligando a dover fare le relative proporzioni degli ingredienti. Se poi si vuole riproporre la stessa ricetta per un numero diverso di persone, tale processo deve essere ripetuto. Da qui l’idea di quanto segue.
+
+Lo scopo principale dell’applicazione è quello di, una volta memorizzata una ricetta, calcolare le quantità dei vari ingredienti proporzionalmente al numero di commensali (scelto dall’utente).
+
+Altre funzionalità offerte dell’applicazione sono:
+
+Generare una lista della spesa per poter essere inviata a un contatto tramite diversi canali (email, whatsapp, …)
+Gestione del proprio ricettario (permanenza ricette)
+Possibilità di modificare ricette, aggiungendo, rimuovendo o cambiando le dosi di ingredienti indipendentemente dalle proporzioni
+Possibilità di aggiungere delle note alle ricette (trucchi, collegamenti a tutorial, osservazioni, …)
+Possibilità di ricercare di una ricetta: sia attraverso filtri tipo portata, tipologia, tempo, difficoltà, … sia per ingredienti (cucina con ciò che hai)
+Conversione tra diversi grassi animali e vegetali (burro-olio, burro-ricotta, …)
+Conversione cucchiaio - grammi e bicchiere - cl
+
+App Rivolta sia a professionisti del settore che a cuochi per passione.
  */
 
 class List_Ricette_Activity : AppCompatActivity(){
